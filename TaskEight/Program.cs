15932fc14d404419d34fe9cb2ac0c4379b6c3b4c
stanislav <stas09091994@gmail.com>
2022-12-04 14:27:04 +0300

    The homework file is working

--- TaskEight/Program.cs
+++ TaskEight/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+using System;
+{
+    Console.WriteLine("Введите число: ");
+    int num = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Все четные числа в диапазоне от 1 до {num}:");
+    for (int i = 1; i <= num; i++)
+        if (i % 2 == 0)
+        {
+            Console.WriteLine(i);
+        }
+    Console.ReadKey();
+}
